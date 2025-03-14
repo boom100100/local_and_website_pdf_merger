040db7e04e131b7f64b620c7140b00c90424a4af
bernadette <bsd231@nyu.edu>
2025-03-14 18:33:48 -0400

    processed request inputs

--- some_script.py
+++ some_script.py
@@ -1,27 +1,51 @@
 import click
+import os
 import sys
 import json
 
 from app.commands.pdf_group import combine_pdfs
 
 
-def main():
-    data=json.loads(sys.argv[1])
-    click_context = click.Context(combine_pdfs)
-
-    # TODO: don't hardcode these
-    count = 1
-    output_directory = "./outputs"
-    # output_directory = "/Users/bernadette/Applications/convert_and_combine_pdfs/outputs"
-    should_use_local_file_paths = False
-    should_use_webpage_urls  = False
-    output_file_name_without_extension = "test output resume"
-    should_open_output_file = True
-    delete_downloaded_files = True
-    webpage_urls = (
-        "https://www.google.com",
+def sanitize_value(value: str) -> str:
+    return "".join(
+        character
+        if character.isalnum() or character == " " 
+        else "_" 
+        for character in value
     )
 
+
+def main():
+    data: dict = json.loads(sys.argv[1])
+
+    env_vars: dict = data.get("env_vars", {})
+
+    count = data.get("-c", 0)
+    output_directory = env_vars.get("OUTPUT_DIRECTORY", "./outputs") # can ignore -d flag, only one input is necessary
+    # "/Users/bernadette/Applications/convert_and_combine_pdfs/outputs"
+
+
+    def set_os_env_var_and_should_use(key: str, default: list) -> bool:
+        # should be false for the chrome extension
+        should_use = False
+        if list_value := env_vars.get(key, default):
+            os.environ[key] = json.dumps(list_value)
+            should_use = True 
+
+        return should_use
+
+    # if this script is running, it can still control env vars
+    should_use_local_file_paths = set_os_env_var_and_should_use("LOCAL_FILE_PATHS", [])
+    should_use_webpage_urls = set_os_env_var_and_should_use("WEBPAGE_URLS", []) # can ignore webpage_urls args
+
+    output_file_name_without_extension = sanitize_value(
+        data.get("-n", "default resume")
+    )
+    should_open_output_file = data.get("-o", True)
+    delete_downloaded_files = data.get("-x", True)
+    webpage_urls = tuple()
+
+    click_context = click.Context(combine_pdfs)
     click_context.invoke(
         combine_pdfs,
         count=count,
