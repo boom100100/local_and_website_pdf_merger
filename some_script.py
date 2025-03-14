e22ef15e9d0bd08af0fd6c52704d52ed5e85fe3f
bernadette <bsd231@nyu.edu>
2025-03-14 15:01:06 -0400

    properly formatted script

--- some_script.py
+++ some_script.py
@@ -1,34 +1,39 @@
 import click
-# import sys
-# import json
+import sys
+import json
 
 from app.commands.pdf_group import combine_pdfs
 
-# data=json.loads(argv[1])
-# def do():
-click_context = click.Context(combine_pdfs)
 
-# TODO: don't hardcode these
-count = 1
-output_directory = "./outputs"
-# output_directory = "/Users/bernadette/Applications/convert_and_combine_pdfs/outputs"
-should_use_local_file_paths = False
-should_use_webpage_urls  = False
-output_file_name_without_extension = "test output resume"
-should_open_output_file = True
-delete_downloaded_files = True
-webpage_urls = (
-    "https://www.google.com",
-)
+def main():
+    data=json.loads(sys.argv[1])
+    click_context = click.Context(combine_pdfs)
 
-click_context.invoke(
-    combine_pdfs,
-    count=count,
-    output_directory=output_directory,
-    should_use_local_file_paths=should_use_local_file_paths,
-    should_use_webpage_urls=should_use_webpage_urls,
-    output_file_name_without_extension=output_file_name_without_extension,
-    should_open_output_file=should_open_output_file,
-    delete_downloaded_files=delete_downloaded_files,
-    webpage_urls=webpage_urls,
-)
+    # TODO: don't hardcode these
+    count = 1
+    output_directory = "./outputs"
+    # output_directory = "/Users/bernadette/Applications/convert_and_combine_pdfs/outputs"
+    should_use_local_file_paths = False
+    should_use_webpage_urls  = False
+    output_file_name_without_extension = "test output resume"
+    should_open_output_file = True
+    delete_downloaded_files = True
+    webpage_urls = (
+        "https://www.google.com",
+    )
+
+    click_context.invoke(
+        combine_pdfs,
+        count=count,
+        output_directory=output_directory,
+        should_use_local_file_paths=should_use_local_file_paths,
+        should_use_webpage_urls=should_use_webpage_urls,
+        output_file_name_without_extension=output_file_name_without_extension,
+        should_open_output_file=should_open_output_file,
+        delete_downloaded_files=delete_downloaded_files,
+        webpage_urls=webpage_urls,
+    )
+
+
+if __name__ == '__main__':
+    main()
