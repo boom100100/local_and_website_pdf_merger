75f863d3a78966f4c7ad83b1a23107b1152f3b91
boom100100 <bsd231@nyu.edu>
2025-03-25 13:24:49 -0400

    updated host install script

--- install_host.bat
+++ install_host.bat
@@ -2,8 +2,8 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file at https://github.com/GoogleChrome/chrome-extensions-samples/blob/main/LICENSE.
 
-cp "%~dp0com.automatedbooks.convert_and_combine_pdfs.json.windows" "%~dp0com.automatedbooks.convert_and_combine_pdfs.json"
-rm "%~dp0com.automatedbooks.convert_and_combine_pdfs.json.windows"
+copy "%~dp0com.automatedbooks.convert_and_combine_pdfs.json.windows" "%~dp0com.automatedbooks.convert_and_combine_pdfs.json"
+del "%~dp0com.automatedbooks.convert_and_combine_pdfs.json.windows"
 
 :: Change HKCU to HKLM if you want to install globally.
 :: %~dp0 is the directory containing this bat script and ends with a backslash.
